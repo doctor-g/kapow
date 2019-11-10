64befd7f5b41220d8852ef7972d050aee920f34f
Paul Gestwicki <pvgestwicki@bsu.edu>
2019-11-10 16:39:33 -0500

    Initial public release

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit with nonzero exit code if anything fails
+set -e 
+
+HANDOUTS="kapow-moves-handout kapow-playbook-handouts kapow-team-handout kapow-narrator-handout"
+
+# Run pdflatex twice on the main rules in order to ensure
+# crossreferences are up to date.
+pdflatex kapow-rules
+pdflatex kapow-rules
+
+# The other files don't have crossreferences and so we should
+# only have to run pdflatex once.
+for source in ${HANDOUTS}; do
+    pdflatex "$source"
+done
